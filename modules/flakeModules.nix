82453c41e9ca6ff9f29ac4cbd97d4e56cedf4761
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-01-18 12:48:52 -0600

    refactor: remove environments

--- modules/flakeModules.nix
+++ modules/flakeModules.nix
@@ -1,11 +1,20 @@
-{ config, ... }:
+{ config, inputs, ... }:
 let
   localModule = {
     # export all flake modules under flake.modules.flake
     flake.flakeModules = config.flake.modules.flake //
       { default = config.flake.modules.flake.systems; };
   };
+
+  flakeModule = let _file = __curPos.file; key = _file; in {
+    inherit _file key;
+
+    imports = [
+      inputs.flake-parts.flakeModules.flakeModules
+    ];
+  };
 in
 {
   imports = [ localModule ];
+  flake.modules.flake.flakeModules = flakeModule;
 }
