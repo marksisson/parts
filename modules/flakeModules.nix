7cc5b71c018feda75a7f4b0122d4d1b29ad88181
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-01-19 17:10:26 -0600

    fix: update flakeModules to be custom module

--- modules/flakeModules.nix
+++ modules/flakeModules.nix
@@ -1,6 +1,42 @@
-{ config, inputs, ... }:
+{ config, flake-parts-lib, inputs, lib, moduleLocation, ... }:
 let
+  flakeModulesOption = with lib; with types; mkOption
+    {
+      type = lazyAttrsOf deferredModule;
+      default = { };
+      apply = mapAttrs (k: v: {
+        _file = "${toString moduleLocation}#flakeModules.${k}";
+        imports = [ v ];
+        _class = "flake";
+      });
+      description = ''
+        flake-parts modules for use by other flakes.
+
+        If the flake defines only one module, it should be `flakeModules.default`.
+      '';
+    };
+
+  module2 = with lib; {
+    options = {
+      flake = mkOption {
+        type = types.submoduleWith {
+          modules = [
+            (mkAliasOptionModule [ "flakeModule" ] [ "flakeModules" "default" ])
+            {
+              options.flakeModules = flakeModulesOption;
+            }
+          ];
+        };
+      };
+    };
+  };
+
+  module = {
+    imports = [ inputs.flake-parts.flakeModules.flakeModules ];
+  };
+
   localModule = {
+    imports = [ module ];
     # export all flake modules under flake.modules.flake
     flake.flakeModules = config.flake.modules.flake //
       { default = config.flake.modules.flake.systems; };
@@ -8,10 +44,7 @@ let
 
   flakeModule = {
     key = "7F193AF6-B12D-4FC7-8473-129F2F787F80";
-
-    imports = [
-      inputs.flake-parts.flakeModules.flakeModules
-    ];
+    imports = [ module ];
   };
 in
 {
