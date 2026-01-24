dcdf2e267ed90531374525ee485078a875df75bc
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-01-23 20:35:31 -0600

    update: make lib reusable

--- modules/lib.nix
+++ modules/lib.nix
@@ -1,28 +1,27 @@
 { inputs, ... }:
 let
+  builtinModule =
+    {
+      imports = [
+        ./components.nix
+        ./meta.nix
+        ./nixology.nix
+      ];
+
+      # make the whole flake module a component
+      nixology.components.flake = module;
+
+      # default systems
+      systems = import inputs.systems;
+
+      # default pkgs
+      perSystem = { system, ... }: {
+        _module.args.pkgs = builtins.seq inputs.nixpkgs inputs.nixpkgs.legacyPackages.${system};
+      };
+    };
+
   module = {
     mkFlake = args: module:
-      let
-        builtinModule =
-          {
-            imports = [
-              ./components.nix
-              ./meta.nix
-              ./nixology.nix
-            ];
-
-            # make the whole flake module a component
-            nixology.components.flake = module;
-
-            # default systems
-            systems = import inputs.systems;
-
-            # default pkgs
-            perSystem = { system, ... }: {
-              _module.args.pkgs = builtins.seq inputs.nixpkgs inputs.nixpkgs.legacyPackages.${system};
-            };
-          };
-      in
       inputs.flake-parts.lib.mkFlake args { imports = [ module builtinModule ]; };
 
     modulesIn = directory: with inputs.nixpkgs.lib; let
