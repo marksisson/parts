ff51596866820ec181c2f32423b1be68e08e7bfd
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-01-29 13:50:03 -0600

    test: toml

--- modules/lib.nix
+++ modules/lib.nix
@@ -1,25 +1,41 @@
-{ inputs, ... }:
+{ inputs, lib ? inputs.nixpkgs.lib, ... }:
 let
-  library = {
-    mkFlake = flakeArgs@{ flakeref, ... }: flakeModule:
-      let
-        args = builtins.removeAttrs flakeArgs [ "flakeref" ];
-        module = { imports = [ flakeModule builtinModule ]; };
-      in
-      inputs.flake-parts.lib.mkFlake args {
-        imports = [ module { flake.meta.flakeref = flakeref; } ];
-      };
+  library =
+    let
+      mkFlake = flakeArgs@{ flakeref, ... }: flakeModule:
+        let
+          args = builtins.removeAttrs flakeArgs [ "flakeref" ];
+          module = { imports = [ flakeModule builtinModule ]; };
+        in
+        inputs.flake-parts.lib.mkFlake args {
+          imports = [ module { flake.meta.flakeref = flakeref; } ];
+        };
 
-    modulesIn = directory: with inputs.nixpkgs.lib; let
-      moduleFiles =
-        if filesystem.pathIsDirectory directory
-        then
-          (filter (n: strings.hasSuffix ".nix" n) (filesystem.listFilesRecursive directory))
-        else
-          [ ];
+      mkTOMLFlake = flakeArgs: tomlFile:
+        let
+          toml = builtins.fromTOML (builtins.readFile tomlFile);
+          args = flakeArgs // { inherit (toml.flake) flakeref; };
+          source = lib.lists.head toml.sources;
+          name = lib.lists.last (lib.strings.split "/" source.url);
+          component = lib.lists.head source.components;
+          input = "${name}.components.${component}";
+          module = lib.getAttrFromPath (lib.strings.splitString "." input) flakeArgs.inputs;
+        in
+        builtins.trace "name=${name}\ncomponent=${component}\ninputs=${input}\n" mkFlake args module;
+
+      modulesIn = directory: with inputs.nixpkgs.lib; let
+        moduleFiles =
+          if filesystem.pathIsDirectory directory
+          then
+            (filter (n: strings.hasSuffix ".nix" n) (filesystem.listFilesRecursive directory))
+          else
+            [ ];
+      in
+      moduleFiles;
     in
-    moduleFiles;
-  };
+    {
+      inherit mkFlake mkTOMLFlake modulesIn;
+    };
 
   builtinModule = { lib, ... }:
     {
@@ -49,7 +65,7 @@ let
   };
 in
 {
-  flake.lib = library; # this is for lib access when imported directly (e.g. in this flake)
+  flake.lib = library; # this is for lib access when imported directly (i.e. in this flake)
   imports = [ module ];
   flake.components.nixology.parts.lib = component;
 }
